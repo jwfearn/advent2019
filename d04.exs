8439bd55a26ec81cb56641098796c506b69fc6f6
jwfearn <jwfearn@yahoo.com>
2019-12-04 00:22:28 -0800

    Day 4, part 2

--- d04.exs
+++ d04.exs
@@ -1,17 +1,17 @@
 defmodule P1 do
-  def count(str) do
+  def count(str, func \\ &ok?/1) do
     [lo, hi] = String.split(str, "-")
     String.to_integer(lo)..String.to_integer(hi)
-    |> Enum.filter(&ok?/1)
+    |> Enum.filter(func)
     |> Enum.count
   end
 
-  defp ok?(n) do
+  def ok?(n) do
     digits = Integer.digits(n)
     increasing?(digits) and repeats?(digits)
   end
 
-  defp increasing?(digits) do
+  def increasing?(digits) do
     digits
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.all?(fn [a, b] -> a <= b end)
@@ -24,5 +24,23 @@ defmodule P1 do
   end
 end
 
+defmodule P2 do
+  def ok?(n) do
+    digits = Integer.digits(n)
+    P1.increasing?(digits) and Enum.member?(run_lengths(digits), 2)
+  end
+
+  defp run_lengths(digits) do
+    digits
+    |> Enum.reduce(%{}, fn digit, map -> Map.put(map, digit, Map.get(map, digit, 0) + 1) end)
+    |> Map.values
+  end
+end
+
+ExUnit.start()
+
 P1.count("146810-612564")
-|> IO.inspect(label: "Part 1 Answer")
+|> IO.inspect(label: "Part 1 Answer") # 1748
+
+P1.count("146810-612564", &P2.ok?/1)
+|> IO.inspect(label: "Part 2 Answer") # 1180
